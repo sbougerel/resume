3bd324f190d236ebdd1ce83a67b8ded6d7253497
Sylvain Bougerel <sylvain.bougerel@gmail.com>
2024-02-01 09:22:16 +0800

    chore(cleanup): removed unused items

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if ! [ -f "./dot.emacs" ] || ! [ -d "./dot.emacs.d" ]
-then
-    >&2 echo "Are you running $0 in the right directory?"
-    exit 1
-fi
-cp dot.emacs $HOME/.emacs
-cp -a dot.emacs.d/* $HOME/.emacs.d/
